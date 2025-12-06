59e1263d748c59ec7b4fc86ea4299d00a806030c
seilahhh <pereirajoaquim312@gmail.com>
2025-12-06 02:44:41 +0000

    Upscale technology icons at about section

--- src/Components/Cards/TechnologyCard.tsx
+++ src/Components/Cards/TechnologyCard.tsx
@@ -10,7 +10,7 @@ function TechnologyCard({ name, icon }: TechnologyCardProps) {
         <div className="flex items-center p-2 rounded-lg shadow-md shadow-primary outline outline-1 outline-primary">
             <span className="text-md">{name}</span>
             {icon && (
-                <img src={`/technologies/${icon}`} alt={`${name} icon`} className="w-5 h-5 ms-2" />
+                <img src={`/technologies/${icon}`} alt={`${name} icon`} className="w-7 h-7 ms-2" />
             )}
         </div>
     );
